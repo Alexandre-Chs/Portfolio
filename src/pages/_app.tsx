47aaf5806a0e8260b8b1ff8a7a7e3bb830d2b374
Alexandre <alexandrechanas0@gmail.com>
2023-04-13 16:02:57 +0200

    Add smooth animation on main page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: ["100", "400", "500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component className={montserrat.className} {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,127 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Link from "next/link";
+import styles from "../styles/font/font.module.css";
+import { useRouter } from "next/router";
+import { FiArrowRight } from "react-icons/fi";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
+  const [showArrow, setShowArrow] = useState([false, false, false]);
+  const [slide, setSlide] = useState(false);
+
+  const router = useRouter();
+
+  const handleClick = (
+    index: number,
+    e: {
+      preventDefault: () => void;
+      currentTarget: any;
+    }
+  ) => {
+    //wowowowo too much setTimeout
+    e.preventDefault();
+    const element = e.currentTarget;
+    element.style.filter = "blur(10px)";
+    element.style.transition = "all .5s ease";
+    setTimeout(() => {
+      element.style.fontSize = "0px";
+      element.style.filter = "blur(0px)";
+      const newShowArrow = [...showArrow];
+      newShowArrow[index] = true;
+      setShowArrow(newShowArrow);
+    }, 400);
+
+    setTimeout(() => {
+      setSlide(true);
+      element.style.transition = "all .8s ease";
+      element.style.transform = "translateX(4000px)";
+      element.style.opacity = "0";
+      element.style.filter = "blur(350px)";
+    }, 1000);
+
+    setTimeout(() => {
+      router.push(element.href);
+      setSlide(false);
+    }, 1700);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="overflow-hidden">
+      <motion.div
+        initial={{
+          opacity: 0,
+          scale: 1.5,
+          filter: "blur(10px)",
+          x: 0,
+        }}
+        animate={{
+          opacity: 1,
+          scale: 1,
+          filter: "blur(0px)",
+          x: slide ? "-100%" : 0,
+          transition: {
+            duration: 0.7,
+            ease: "easeInOut",
+          },
+        }}
+        // transition={{ duration: 0.6, delay: 0.2 }}
+        className="flex h-screen bg-background"
+      >
+        <div className="basis-1/12"></div>
+        <div className="flex items-end justify-start basis-4/12 pb-[15em]">
+          <div className="font-medium max-w-[80%]">
+            <h1 className="pb-4 tracking-wider text-l">ALEXANDRE CHANAS</h1>
+            <p className="text-sm font-normal leading-relaxed">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+              itaque minus. Beatae dolores iure non placeat ipsam temporibus
+              accusantium recusandae soluta? Alias quod soluta deserunt ea unde
+              ducimus sit facere!
+            </p>
+            <p>Current working to chez moi</p>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+        <div className="flex flex-col items-start justify-center basis-4/6 min-h-[100px]">
+          <Link
+            onClick={(e) => handleClick(0, e)}
+            className={styles.title}
+            href="/about"
           >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+            A propos
+            {showArrow[0] == true ? (
+              <FiArrowRight
+                size={150}
+                className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              />
+            ) : null}
+          </Link>
+          <Link
+            onClick={(e) => handleClick(1, e)}
+            className={styles.title}
+            href="/"
           >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+            Projets
+            {showArrow[1] == true ? (
+              <FiArrowRight
+                size={100}
+                className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              />
+            ) : null}
+          </Link>
+          <Link
+            onClick={(e) => handleClick(2, e)}
+            className={styles.title}
+            href="/"
           >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            Contact
+            {showArrow[2] == true ? (
+              <FiArrowRight
+                size={100}
+                className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              />
+            ) : null}
+          </Link>
+        </div>
+      </motion.div>
+    </div>
+  );
 }
